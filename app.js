289a3fd8ce6c5d91d26256db31a639c3920e6f91
nick-terrant <86777926+nick-terrant@users.noreply.github.com>
2024-08-01 18:00:51 +0100

    Update app.js
    firebase config added

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    // Your config object here
-    // apiKey: "YOUR_API_KEY",
-    // authDomain: "YOUR_AUTH_DOMAIN",
-    // projectId: "YOUR_PROJECT_ID",
-    // ...
+  apiKey: "AIzaSyD1PHa_c_DzrbK_Zjt2hO4lwSdhNgZMFTo",
+  authDomain: "smart-dpia.firebaseapp.com",
+  projectId: "smart-dpia",
+  storageBucket: "smart-dpia.appspot.com",
+  messagingSenderId: "755471262348",
+  appId: "1:755471262348:web:104795a08707a02f95d1a9",
+  measurementId: "G-0SCK0RT47C"
 };
 
 // Initialize Firebase
